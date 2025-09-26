3a905c4e456377c3f7b849554ef6025ed6c8c1b9
seunghyun lee <lee_tmdgus@naver.com>
2025-09-26 18:51:55 +0900

    우하하

--- qr_maker/settings.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import os
-from dotenv import load_dotenv
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env")
-
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "insecure-key")
-DEBUG = os.environ.get("DEBUG", "False") == "True"
